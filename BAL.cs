38c4f9a1465aea720b08348d227e1906ac26b776
asif <iu.asifak@gmail.com>
2017-12-18 00:50:13 -0800

    Selected files of RPS

---
+++ BAL.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Data.Entity;
+using System.Text.RegularExpressions;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.SqlServer;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace RPS
+{
+    static class Smatch
+    {
+        public static bool IsValid(string value)
+        {
+            return Regex.IsMatch(value, @"(MF|JS)([0-9]{2})(MSC|PHD)([0-9]{4})", RegexOptions.Singleline);
+        }
+    }
+
+    [Table("TableA")]
+    class BAL
+    {
+        [Key]
+        [MaxLength(20)]
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        public String Userid { get; set; }
+        [MinLength(5)]
+        [MaxLength(20)]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        public string Pass { get; set; }
+    }
+    [Table("TableB")]
+    class AddReviewerBAL : IDisposable
+    {
+        [Key]
+        public int Reviewerid { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DTAppoint { get; set; }
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(15, ErrorMessage = "Initials cannot exceed 15 characters")]
+        public string Initials { get; set; }
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "Full Name cannot exceed 50 characters")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "The entry must be a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(10, ErrorMessage = "Qualificaton cannot exceed 10 characters")]
+        public string Qualification { get; set; }
+
+        [MaxLength(10, ErrorMessage = "Channel cannot exceed 10 characters")]
+        public string Channel { get; set; }//internal / external
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(25, ErrorMessage = "Type of Reviewer cannot exceed 25 characters")]//editorial board member, independent reviewer
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(25, ErrorMessage = "Designation cannot exceed 25 characters")]
+        public string Designation { get; set; }
+        //[Required(ErrorMessage = "Cannot be left Empty")]
+        //[MaxLength(250, ErrorMessage = "Expertise cannot exceed 250 characters")]
+        //public List<string> Expertise { get; set; }
+        //public bool HaveAdd { get; set; }
+
+        public virtual RevOfficeAdd RevOfficeAdd { get; set; }
+        public virtual RevHomeAdd RevHomeAdd { get; set; }
+        public ICollection<Expertise> Expertise { get; set; }
+
+        public void Dispose()
+        {
+
+        }
+    }
+    [Table("TableC")]
+    class RevOfficeAdd : IDisposable
+    {
+        [ForeignKey("AddReviewerBAL")]
+        public int RevOfficeAddid { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "Institute Name cannot exceed 50 characters")]
+        public string Institute { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "Street Address cannot exceed 50 characters")]
+        public string StreetAdd { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "City Name cannot exceed 50 characters")]
+
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "State / Province Name cannot exceed 50 characters")]
+
+        public string StateProvince { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "Country Name cannot exceed 50 characters")]
+        public string Country { get; set; }
+
+
+        [MaxLength(15, ErrorMessage = "Landline cannot exceed 15 characters")]
+        public string OffLandline { get; set; }
+
+        [MaxLength(15, ErrorMessage = "Cell Number cannot exceed 15 characters")]
+        public string OffCell { get; set; }
+        public virtual AddReviewerBAL AddReviewerBAL { get; set; }
+
+        public void Dispose()
+        {
+
+        }
+    }
+    [Table("TableD")]
+    class RevHomeAdd : IDisposable
+    {
+        [ForeignKey("AddReviewerBAL")]
+        public int RevHomeAddid { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "Street Address cannot exceed 50 characters")]
+        public string StreetAdd { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "City Name cannot exceed 50 characters")]
+
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "State / Province Name cannot exceed 50 characters")]
+
+        public string StateProvince { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(50, ErrorMessage = "Country Name cannot exceed 50 characters")]
+        public string Country { get; set; }
+
+
+        [MaxLength(15, ErrorMessage = "Landline cannot exceed 15 characters")]
+        public string HomeLandline { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(15, ErrorMessage = "Cell Number cannot exceed 15 characters")]
+        public string Cell { get; set; }
+        public virtual AddReviewerBAL AddReviewerBAL { get; set; }
+
+        public void Dispose()
+        {
+
+        }
+    }
+    [Table("TableE")]
+    class Expertise : IDisposable
+    {
+        [Key]
+        public int Expertiseid { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left Empty")]
+        [MaxLength(75, ErrorMessage = "Experty cannot exceed 75 characters")]
+        public string Expertin { get; set; }
+
+        //[ForeignKey("AddReviewerBAL")]
+        public int Reviewerid { get; set; }
+        public AddReviewerBAL AddReviewerBAL { get; set; }
+
+        public void Dispose()
+        {
+
+        }
+    }
+    [Table("TableF")]
+    class PAuthor : IDisposable
+    {
+        [Key]
+        public int Authorid { get; set; }
+        [Required, MaxLength(50)]
+        public string PauthorName { get; set; }
+        [Required, MaxLength(150)]
+        public string PauthorAff { get; set; }
+        [Required, MaxLength(50), DataType(DataType.EmailAddress)]
+        public string Pauthoremail { get; set; }
+        [Required, MaxLength(15)]
+        public string Pauthorcell { get; set; }
+
+        public ICollection<Article> Article
+        {
+            get; set;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+    [Table("TableG")]
+    class CAuthor // Not Implemented
+    {
+        [Key]
+        public int CAuthorid { get; set; }
+        [Required, MaxLength(50)]
+        public string CauthorName { get; set; }
+        [MaxLength(150)]
+        public string CauthorAff { get; set; }
+        [Required, MaxLength(50), DataType(DataType.EmailAddress)]
+        public string Cauthoremail { get; set; }
+        [MaxLength(15)]
+        public string Cauthorcell { get; set; }
+        public string ArticleID { get; set; }
+        public Article Article { get; set; }
+    }
+    public enum Articlestatus : Byte
+    {
+        Inplagiarism = 0,
+        Consentletter = 1,
+        Inreview = 2,
+        AuthorRevision = 4,
+        ReReview = 8,
+        Reject = 16,
+        Accept = 32,
+        Published = 64
+    }
+    [Table("TableH")]
+    class Article : IDisposable
+    {
+
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string ArticleID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DTsubmission { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Title { get; set; }
+        [Required, EnumDataType(typeof(Articlestatus))]
+        public Articlestatus Articlestatus
+        {
+            get; set;
+        }
+        public int Authorid { get; set; }
+        public PAuthor PAuthor { get; set; }
+        public ICollection<CAuthor> CAuthor { get; set; }
+
+        public void Dispose()
+        {
+
+        }
+    }
+    //[Table("TableI")]
+    //class Payment
+    //{ }
+    class Conn : DbContext
+    {
+        public Conn() : base("cs")
+        {
+
+        }
+
+
+        public DbSet<BAL> LoginBals
+        {
+            get; set;
+        }
+        public DbSet<AddReviewerBAL> AddReviewerBALs { get; set; }
+        public DbSet<RevOfficeAdd> RevOfficeAdds { get; set; }
+        public DbSet<RevHomeAdd> RevHomeAdds { get; set; }
+        public DbSet<Expertise> Expertises { get; set; }
+
+        public DbSet<PAuthor> PAuthors { get; set; }
+
+        public DbSet<CAuthor> CAuthors { get; set; }
+        public DbSet<Article> Articles { get; set; }
+
+    }
+}
---
+++ IREPO.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+namespace RPS
+{
+    interface IREPO<T> where T : class
+    {
+        List<T> getall();
+        
+        bool getallbyid(T a);  // for login
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
+        T insert(T a);
+        T update(T a);
+        T delete(T a);
+
+    }
+    class GRepo<T> : IREPO<T>, IDisposable where T : class
+    {
+        private Conn cc;
+        private DbSet<T> entities;
+        public GRepo()
+        {
+            cc = new Conn();
+            entities = cc.Set<T>();
+        }
+        //public GRepo(Conn c)
+        //{
+        //    this.cc = c;
+        //    entities = cc.Set<T>();
+        //}
+        public T delete(T a)
+        {
+            entities.Remove(a);
+            cc.SaveChanges();
+            return a;
+        }
+
+
+        public IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            IQueryable<T> query = cc.Set<T>().Where(predicate);
+            return query;
+        }
+
+        public List<T> getall()
+        {
+            return entities.ToList();
+
+        }
+
+        public bool getallbyid(T a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T insert(T a)
+        {
+            entities.Add(a);
+            try
+            {
+                cc.SaveChanges();
+                return a;
+            }
+            catch (Exception)
+            {
+                
+                throw new System.Data.Entity.Core.UpdateException();
+            }
+        }
+
+        public T update(T a)
+        {
+            cc.Entry(a).State = EntityState.Modified;
+            cc.SaveChanges();
+            return a;
+        }
+        ~GRepo()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            cc.Dispose();
+        }
+    }
+    class LoginREpo : IREPO<BAL>// For login only
+    {
+        Conn cn = new Conn();
+
+        public BAL delete(BAL a)
+        {
+
+            var v = cn.LoginBals.Find(a.Userid);
+            cn.LoginBals.Remove(v);
+            cn.SaveChanges();
+            return a;
+        }
+
+        ~LoginREpo() { }
+
+        public IQueryable<BAL> FindBy(Expression<Func<BAL, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BAL> getall()
+        {
+            var x = cn.LoginBals.ToList();
+            return x;
+        }
+
+        public bool getallbyid(BAL a)
+        {
+            try
+            {
+                var x = cn.LoginBals.Any(b => b.Userid == a.Userid && b.Pass == a.Pass);
+                return x;
+            }
+
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                //return false;
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public BAL insert(BAL a)
+        {
+            cn.LoginBals.Add(a);
+            cn.SaveChanges();
+            return a;
+
+        }
+
+        public BAL update(BAL a)
+        {
+            // var x = cn.LoginBals.Find(a);
+            cn.Entry(a).State = EntityState.Modified;
+            cn.SaveChanges();
+            return a;
+        }
+    }
+}
